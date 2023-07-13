7d8810088c4f4705ab1366206ae49d1d398c440b
SantiG95 <santiguerra86@gmail.com>
2023-07-13 12:51:38 -0300

    Initial commit

---
+++ clima.js
@@ -0,0 +1,64 @@
+var inputCiudad = document.getElementById("ciudadInput")
+var gpsButton = document.getElementById("gps")
+var enviarButton = document.getElementById("enviar")
+var ubicacionClima = document.getElementById("ubicacionClima")
+var condicionCielo = document.getElementById("condicionCielo")
+var temperatura = document.getElementById("temperatura")
+var velocidadViento = document.getElementById("velocidadViento")
+var informacionClima = document.getElementById("informacionClima")
+
+var posicion;
+
+const weatherAPI = "18adba3bc5141e5f5c3da4e751534d25"
+//const weatherAPI = "3045dd712ffe6e702e3245525ac7fa38"
+
+function obtenerLocalizacion(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(obtenerInformacionClimaPorGPS)
+    }
+}
+
+function obtenerInformacionClimaPorGPS(posicionObtenida){
+    posicion = posicionObtenida
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + posicionObtenida.coords.latitude + "&lon=" + posicionObtenida.coords.longitude + "&appid=" + weatherAPI + "&lang=es")
+    .then(res => res.json())
+    .then(data => pronostico = data)
+    .then(data => {
+            ubicacionClima.innerHTML = "\n<p> El clima en " + data.name + "</p> \n";
+            condicionCielo.innerHTML = "Condicion del cielo: " + data.weather[0].description;
+            temperatura.innerHTML = "Temperatura: " + (data.main.temp - 273).toFixed(1) + "º";
+            velocidadViento.innerHTML = "Velocidad del viento: " + (data.wind.speed * 3.6).toFixed(1) + " km/h";
+            informacionClima.style.visibility = "visible"
+            }
+        )
+    .catch(error => {
+        informacionClima.style.visibility = "hidden"
+        alert("No se ha encontrado la ciudad especificada")
+    })
+}
+
+gpsButton.addEventListener("click", () =>{
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(obtenerInformacionClimaPorGPS)
+    }
+    }
+)
+
+enviarButton.addEventListener('click', () => {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + inputCiudad.value + "&appid=" + weatherAPI + "&lang=es")
+    .then(res => res.json())
+    .then(data => pronostico = data)
+    .then(data => {
+            ubicacionClima.innerHTML = "\n<p> El clima en " + data.name + "</p> \n";
+            condicionCielo.innerHTML = "Condicion del cielo: " + data.weather[0].description;
+            temperatura.innerHTML = "Temperatura: " + (data.main.temp - 273).toFixed(1) + "º";
+            velocidadViento.innerHTML = "Velocidad del viento: " + (data.wind.speed * 3.6).toFixed(1) + " km/h";
+            informacionClima.style.visibility = "visible"
+            }
+        )
+    .catch(error => {
+        informacionClima.style.visibility = "hidden"
+        alert("No se ha encontrado la ciudad especificada")
+    })
+    }
+)
